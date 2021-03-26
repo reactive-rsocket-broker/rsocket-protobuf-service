7566bf5229337b3c94f5b2976ade18dec93bc064
linux_china <libing.chen@gmail.com>
2021-03-26 01:02:54 -0700

    RsocketProtobufServer

---
+++ src/main/java/com/alibaba/rsocket/protobuf/RsocketProtobufServer.java
@@ -0,0 +1,13 @@
+package com.alibaba.rsocket.protobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketProtobufServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketProtobufServer.class, args);
+    }
+
+}
