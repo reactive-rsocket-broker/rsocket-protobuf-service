ce8f117963b2402478e2ba73014af260c816e413
linux_china <libing.chen@gmail.com>
2021-04-03 15:23:16 -0700

    Remove AccountProtoService bean

--- account-service-spring-boot-starter/src/main/java/com/alibaba/account/springboot/AccountServicesAutoConfiguration.java
+++ account-service-spring-boot-starter/src/main/java/com/alibaba/account/springboot/AccountServicesAutoConfiguration.java
@@ -9,17 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Configuration
 public class AccountServicesAutoConfiguration {
-    @Bean
-    public AccountProtoService accountService(UpstreamManager upstreamManager) {
-        return RSocketRemoteServiceBuilder
-                .client(AccountProtoService.class)
-                .encodingType(RSocketMimeType.Protobuf)
-                .upstreamManager(upstreamManager)
-                .build();
-    }
 
     @Bean
     public AccountProto2Service accountProto2Service(UpstreamManager upstreamManager) {
