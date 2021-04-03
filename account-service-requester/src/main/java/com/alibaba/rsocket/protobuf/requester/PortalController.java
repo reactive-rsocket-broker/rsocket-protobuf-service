1262d14a4abe5729fb1f71eae84cb2e48ac78749
linux_china <libing.chen@gmail.com>
2021-04-03 15:25:25 -0700

    introduce AccountProto2Service

--- account-service-requester/src/main/java/com/alibaba/rsocket/protobuf/requester/PortalController.java
+++ account-service-requester/src/main/java/com/alibaba/rsocket/protobuf/requester/PortalController.java
@@ -1,7 +1,6 @@
 package com.alibaba.rsocket.protobuf.requester;
 
-import com.alibaba.account.AccountProtoService;
-import com.google.protobuf.Int32Value;
+import com.alibaba.account.AccountProto2Service;
 import com.google.protobuf.util.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +12,11 @@ import reactor.core.publisher.Mono;
 @RestController
 public class PortalController {
     @Autowired
-    AccountProtoService accountProtoService;
+    AccountProto2Service accountProtoService;
 
     @GetMapping(value = "/account/{id}", produces = "application/json")
     public Mono<String> showAccount(@PathVariable("id") Integer id) {
-        return accountProtoService.findById(Int32Value.of(id)).handle((account, sink) -> {
+        return accountProtoService.findById(id).handle((account, sink) -> {
             try {
                 sink.next(JsonFormat.printer().print(account));
             } catch (Exception e) {
